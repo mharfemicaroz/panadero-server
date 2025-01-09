b097129323c1f16f14487b22b67a75f4462b8154
unknown <mharfemicaroz@gmail.com>
2025-01-09 23:46:06 +0800

    update corspolicy

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 initializeFirebase();
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://panadero-server.area51.ph"],
+  origin: ["http://localhost:5173", "https://panadero.area51.ph"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
