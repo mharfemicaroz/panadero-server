638e03f7d94a3261be6ae77227529a72d3ead348
unknown <mharfemicaroz@gmail.com>
2025-03-03 17:15:54 +0800

    upgrade server

--- faceApiSetup.js
+++ faceApiSetup.js
@@ -1,10 +1,6 @@
 // server/faceApiSetup.js
 
-<<<<<<< HEAD
 require("@tensorflow/tfjs-node"); // speeds up TF on Node
-=======
-// require("@tensorflow/tfjs-node"); // speeds up TF on Node
->>>>>>> 5d599e5abe490abc467f68f0fef3998953d5e14b
 const canvas = require("canvas");
 const faceapi = require("@vladmandic/face-api");
 
